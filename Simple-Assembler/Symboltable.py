6eb5cdb27fbed06a2ab50259daf2df10e7b59a05
amit20493 <amit20493@iiitd.ac.in>
2021-08-10 13:09:50 +0530

    edited

--- Simple-Assembler/Symboltable.py
+++ Simple-Assembler/Symboltable.py
@@ -14,9 +14,11 @@ def addVariable(variable,location):
     symboltable.append({variable:["variable",location]})
 def addLabel(label,location):
     symboltable.append({label:["label",location]})
+
+
     
 #valid variable to be declared in main file
-#validlabe to be declared in main file
+#validlabel to be declared in main file
 
 registers = {"R0":"000","R1":"001","R2":"010","R3":"011","R4":"100","R5":"101","R6":"110","FLAGS":"111"}
 #DO NOT CHANGE
