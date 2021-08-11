4f28985a24287016adc6b0dfad0f97f682599997
Nik <nikhil20530@iiitd.ac.in>
2021-08-11 11:04:30 +0530

    line no. added

--- Simple-Assembler/error_type.py
+++ Simple-Assembler/error_type.py
@@ -1,20 +1,20 @@
-def error_code(code):
+def error_code(code,line):
     if code==-1:
-        return print("hlt not being used as the last instruction")
+        return print("hlt not being used as the last instruction in line"+ line )
     elif code==-2:
-        return print("Missing hlt instruction")
+        return print("Missing hlt instruction in line"+ line)
     elif code==-3:
-        return print("Variables not declared at the beginning")
+        return print("Variables not declared at the beginning in line"+ line)
     elif code==-4:
-        return print("Syntax error")     #doubt
+        return print("Syntax error in line"+ line)     #doubt
     elif code==-5:
-        return print("Wrong syntax used") #IMP
+        return print("Wrong syntax used in line"+ line) #IMP
     elif code==-6:
-        return print("Wrong var type provided") 
+        return print("Wrong var type provided in line"+ line) 
     elif code==-7:
-        print("Opcode cannot be used as a variable")
+        print("Opcode cannot be used as a variable in line"+ line)
     elif code==-8:
-        print("Cannot fit program in given space (bigger than 255 lines)")     
+        print("Cannot fit program in given space (bigger than 255 lines) in line"+ line)     
     elif code==-9:
         print("Syntax Error")
     elif code==-10:
