3a19884baa7385709d00cffc306ee9cac1e4c181
Vishesh Jain <jainvishesh27@gmail.com>
2021-08-11 13:30:19 +0530

    Session 11Aug morning

--- Simple-Assembler/error_type.py
+++ Simple-Assembler/error_type.py
@@ -6,9 +6,9 @@ def error_code(code,line):
     elif code==-3:
         return print("Variables not declared at the beginning in line"+ line)
     elif code==-4:
-        return print("Syntax error in line"+ line)     #doubt
+        return print("Syntax error in line "+ str(line))     #doubt
     elif code==-5:
-        return print("Wrong syntax used in line"+ line) #IMP
+        return print("Wrong syntax used in line "+  str(line)) #IMP
     elif code==-6:
         return print("Wrong var type provided in line"+ line) 
     elif code==-7:
@@ -18,7 +18,7 @@ def error_code(code,line):
     elif code==-9:
         print("Syntax Error")
     elif code==-10:
-        print("Extra operand provided")
+        print("Extra operand provided "+str(line))
     elif code==-11:
         print("Needs more operands") 
     elif code==-12:
@@ -26,7 +26,7 @@ def error_code(code,line):
     elif code==-13:
         print("called in middle of program(not allowed) ")     
     elif code==-14:
-        print("Not found")
+        print("Not found"+str)
     elif code==-15:
         print("Lable not initialised")
     elif code==-16:
--- Simple-Assembler/main.py
+++ Simple-Assembler/main.py
@@ -5,34 +5,81 @@ import error_type
 from sys import stdin
 
 
+def getLineIndex(line):
+    return original_file_list.index(line)+1
+
+
 def passOne():
     global numberOfVars
     numberOfVar=0
     for line in Lines:
-        if(" " in line):
-            error_type(-4)
+        if("" in line or ':' in line):
+            error_type.error_code(-4,getLineIndex(' '.join(line)))
             exit()
-            error=-9 #-4
-            break
         elif(line[0]=="var"):
             if(len(line)>2):
-                error_type(-5)
+                error_type.error_code(-4,getLineIndex(' '.join(line)))
                 exit()
-                print("error") #-5
             elif((line[1] in Symboltable.symboltable) or Symboltable.isImm(line[1]) or (line[1] in Symboltable.registers)):
-                error_type(-6)
+                error_type.error_code(-6,getLineIndex(' '.join(line)))
                 exit()
-                print("error") #-6
             else:
                 numberOfVar+=1
-                Symboltable.addVariable(line[1],hlt_pos+numberOfVar)        
+                Symboltable.addVariable(line[1],hlt_pos+numberOfVar)   
+        elif(":" in line[0]):        #[['label:','add','R0','R1','R3']]
+            label_name=line[0][0:line[0].index(":")]
+            if(label_name in Symboltable.symboltable or label_name in Symboltable.registers or Symboltable.isImm(label_name) or (label_name=="var") or (label_name in op.opcode_table)):
+                error_type.error_code(-4,getLineIndex(' '.join(line)))
+                exit()
+            else:
+                 Symboltable.addLabel(label_name+":",Lines.index(line)-numberOfVar)   
+
+
+
+def passTwo():
+    program_counter=0
+    for i in range(count_var,len(Lines)):
+        line_list=Lines[i];    
+        if(line_list[0] not in op.opcode_table):
+            if(line_list[0] not in Symboltable.symboltable):
+                error_type.error_code(-4,getLineIndex(' '.join(line_list)))
+            elif(Symboltable.symboltable[line_list[0]][0]=="variable"):
+                error_type.error_code(-4,getLineIndex(' '.join(line_list)))   
+            else:
+                program_counter+=1   #We are on the label
+                if(line_list[1] not in op.opcode_table):
+                    print("here")
+                    error_type.error_code(-4,getLineIndex(' '.join(line_list)))
+                else:
+                    checkInstruction(line_list)
+        else:
+            program_counter+=1  
+            checkInstruction(line_list)
+
+
+def checkInstruction(line_list):
+    if(":" in line_list[0]):
+        temp_line=line_list[1:len(line_list)]
+    else:
+        temp_line=line_list    
+    operation=temp_line[0]
+    if(len(temp_line)-1!=op.opcode_table[operation][1]):
+        print(temp_line)
+        error_type.error_code(-10,getLineIndex(' '.join(line_list)))
+
 
 def main():
-    global program_counter,Lines,error,numberOfLines,hlt_pos,count_var
+    global program_counter,Lines,error,numberOfLines,hlt_pos,count_var,original_file_list
     file1 = open('Simple-Assembler\inputfile.txt', 'r')
     #Lines = file1.readlines("\n")
     Lines= file1.read().splitlines()
-    count_hlt=0    
+    file1 = open('Simple-Assembler\inputfile.txt', 'r')
+    original_file_list=file1.read().splitlines() 
+    for line in original_file_list:
+            if("//" in line and line[0]!=" " and line[0]!="/"):
+                updated_line=line[0:line.index("/")].strip()
+                original_file_list[original_file_list.index(line)]=updated_line 
+    count_hlt=0 
     while("" in Lines):
         Lines.remove("")
     for line in Lines:
@@ -49,13 +96,11 @@ def main():
                 Lines[Lines.index(line)]=updated_line 
 
         if("hlt" in Lines[0:numberOfLines-1]  ):
-            error_type(-1)
+            error_type.error_code(-1,getLineIndex("hlt"))
             exit()
-            print("Error as hlt not in end ") #-1
         elif Lines[-1]!='hlt':
-            error_type(-2)
+            error_type.error_code(-2,"")
             exit()
-            print("hlt not in last")   #-2
         else:
             hlt_pos= Lines.index('hlt')   
 
@@ -70,15 +115,15 @@ def main():
                 break
         for line in Lines:
             if(line[0]=="var" and Lines.index(line)>=count_var):
-                error_type(-3)
+                error_type.error_code(-3,getLineIndex(line))
                 exit()
-                print("var in between error")  #-3
-        hlt_pos-=count_var       
-        print(hlt_pos)
-        passOne();  
-        print(Lines)        
+        hlt_pos-=count_var      
+        print(Lines)
+        passOne()          
         print(Symboltable.symboltable)
+        passTwo()
 
 if __name__=="__main__":
     main()
     
+
